0ed27ec9d8e79d1ceea0844bfd68b00e37a0e4b2
zyron-legacy <zyronlegacy@gmail.com>
2026-07-19 01:13:32 +0530

    update branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,24 +14,22 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Z.ai Code Scaffold - AI-Powered Development",
-  description: "Modern Next.js scaffold optimized for AI-powered development with Z.ai. Built with TypeScript, Tailwind CSS, and shadcn/ui.",
+  title: "checkin zyron legacy",
+  description: "Zyron Legacy is a Web3  Game and Gaming Hub rewarding games, earn tokens , and developers showcase own web3 games",
   keywords: ["Z.ai", "Next.js", "TypeScript", "Tailwind CSS", "shadcn/ui", "AI development", "React"],
-  authors: [{ name: "Z.ai Team" }],
-  icons: {
-    icon: "https://z-cdn.chatglm.cn/z-ai/static/logo.svg",
-  },
+  authors: [{ name: "krypt lab" }],
+ 
   openGraph: {
-    title: "Z.ai Code Scaffold",
-    description: "AI-powered development with modern React stack",
-    url: "https://chat.z.ai",
-    siteName: "Z.ai",
+    title: "checkin zyron legacy",
+    description: "Zyron Legacy is a Web3  Game and Gaming Hub rewarding games, earn tokens , and developers showcase own web3 games",
+    url: "https://checkin-zyronlegacy.vercel.app/",
+    siteName: "zyron legacy",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Z.ai Code Scaffold",
-    description: "AI-powered development with modern React stack",
+    title: "checkin zyron legacy",
+    description: "Zyron Legacy is a Web3  Game and Gaming Hub rewarding games, earn tokens , and developers showcase own web3 games",
   },
 };
 
